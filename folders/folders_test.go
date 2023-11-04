011356247d95c2a67e1a7dfa2032841fc2ecb16a
Elijah-Bradford <99620295+Elijah-Bradford@users.noreply.github.com>
2023-11-05 10:34:51 +1100

    Written Unit Tests for GetAllFolders

--- folders/folders_test.go
+++ folders/folders_test.go
@@ -2,12 +2,54 @@ package folders_test
 
 import (
 	"testing"
-	// "github.com/georgechieng-sc/interns-2022/folders"
-	// "github.com/stretchr/testify/assert"
+
+	"github.com/georgechieng-sc/interns-2022/folders"
+	"github.com/gofrs/uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_GetAllFolders(t *testing.T) {
-	t.Run("test", func(t *testing.T) {
-		// your test/s here
+	t.Run("Test with valid OrgID", func(t *testing.T) {
+		// Create a sample FetchFolderRequest
+		req := &folders.FetchFolderRequest{
+			OrgID: uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"),
+		}
+
+		// Call the GetAllFolders function
+		response, err := folders.GetAllFolders(req)
+
+		//Assert No Errors Returned
+		assert.NoError(t, err)
+		//Assert Response is not nil
+		assert.NotNil(t, response)
+		assert.NotNil(t, response.Folders)
+		//Assert all 666 Folders returned
+		assert.Equal(t, 666, len(response.Folders))
+	})
+
+	t.Run("Test with invalid OrgID", func(t *testing.T) {
+		// Create a FetchFolderRequest with an invalid OrgID
+		invalidReq := &folders.FetchFolderRequest{
+			OrgID: uuid.FromStringOrNil("invalid_org_id"),
+		}
+
+		// Call the GetAllFolders function with an invalid OrgID
+		response, _ := folders.GetAllFolders(invalidReq)
+
+		//Assert that no Folders are returned
+		assert.Empty(t, response.Folders)
+	})
+
+	t.Run("Test with no OrgID", func(t *testing.T) {
+		//Create invalid request with no uuid
+		invalidReq := &folders.FetchFolderRequest{}
+
+		//Call the GetAllFolders function with invalid request
+		response, err := folders.GetAllFolders(invalidReq)
+
+		//Assert that an Error has been returned
+		assert.Error(t, err)
+		//Assert that the response is empty
+		assert.Empty(t, response)
 	})
 }
